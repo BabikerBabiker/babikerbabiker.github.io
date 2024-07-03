11efd1b59dbd5ea0146cec32e86bf4ca67b7aceb
Babiker Babiker <bbabiker09@gmail.com>
2024-07-03 10:53:38 -0700

    fixed sync

--- sync.sh
+++ sync.sh
@@ -3,15 +3,25 @@
 # Navigate to the local repository
 cd ~/Desktop/GitHub/Portfolio
 
-# Fetch the latest changes from the public repository
+# Fetch the latest changes from the private repository
 git fetch origin
-
-# Merge the changes from the public repository
 git merge origin/main
 
 # Check for merge conflicts
 if [ $? -ne 0 ]; then
-    echo "Merge conflicts detected. Please resolve them and then run this script again."
+    echo "Merge conflicts detected from origin. Please resolve them and then run this script again."
+    exit 1
+fi
+
+# Fetch the latest changes from the public repository
+git fetch https://github.com/babikerbabiker/babikerbabiker.github.io.git
+
+# Merge the changes from the public repository
+git merge FETCH_HEAD
+
+# Check for merge conflicts
+if [ $? -ne 0 ]; then
+    echo "Merge conflicts detected from public repository. Please resolve them and then run this script again."
     exit 1
 fi
 
