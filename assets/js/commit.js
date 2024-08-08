7de398c1f710d902234aaceee8d95ecc0dbc0a43
Babiker Babiker <157412784+BabikerBabiker@users.noreply.github.com>
2024-08-07 19:48:16 -0700

    Added last updated dates for projects and site

--- assets/js/commit.js
+++ assets/js/commit.js
@@ -1,17 +1,39 @@
-async function fetchLastCommitDate() {
-    const username = 'babikerbabiker';
-    const repo = 'babikerbabiker.github.io';
-    const apiUrl = `https://api.github.com/repos/${username}/${repo}/commits`;
+async function fetchCommitDateForElement(element) {
+  const username = 'babikerbabiker';
+  const repo = element.getAttribute('data-repo');
+  const apiUrl = `https://api.github.com/repos/${username}/${repo}/commits?timestamp=${new Date().getTime()}`;
 
-    try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      const lastCommitDate = new Date(data[0].commit.author.date);
-      document.getElementById('last-updated').textContent = lastCommitDate.toDateString();
-    } catch (error) {
-      console.error('Error fetching the last commit date:', error);
-      document.getElementById('last-updated').textContent = 'Error fetching date';
+  console.log('Fetching commits from:', apiUrl);
+
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+
+    const data = await response.json();
+
+    if (data.length === 0) {
+      throw new Error('No commits found for this repository.');
+    }
+
+    const lastCommitDate = new Date(data[0].commit.author.date);
+    element.textContent = lastCommitDate.toDateString();
+  } catch (error) {
+    console.error('Error fetching the last commit date:', error);
+    element.textContent = 'Error fetching date';
+  }
+}
+
+async function fetchAllCommitDates() {
+  const projectElements = document.querySelectorAll('.project');
+  for (const element of projectElements) {
+    await fetchCommitDateForElement(element);
   }
 
-  fetchLastCommitDate();
+  const footerElement = document.querySelector('.last-updated');
+  await fetchCommitDateForElement(footerElement);
+}
+
+window.addEventListener('load', fetchAllCommitDates);
